2c5e108ef77aadbe611d6793e576d4c996f83347
GU1LT <77740294+GU1LT@users.noreply.github.com>
2021-04-25 23:32:54 +0200

    done 1 en 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(" 1 done");
+        System.out.println("2 done ");
     }
 }
