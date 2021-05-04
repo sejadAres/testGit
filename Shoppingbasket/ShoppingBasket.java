c116a663f22c1f31ac53a7ff7a5140138b16390f
GU1LT <77740294+GU1LT@users.noreply.github.com>
2021-05-04 11:30:34 +0200

    shoppingbasket class and directory

---
+++ Shoppingbasket/ShoppingBasket.java
@@ -0,0 +1,16 @@
+public class ShoppingBasket {
+    private Product products;
+
+    public ShoppingBasket(Product products) {
+        this.products = products;
+    }
+
+    public Product getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product products) {
+        this.products = products;
+    }
+
+}
